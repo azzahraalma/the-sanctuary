fd7e84e3dd4c198501c6401f2be05399ddd30f6d
alma <almaliaazzhr@gmail.com>
2026-05-09 20:29:49 +0700

    initial commit The Sanctuary

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h1>The Sanctuary</h1>
+      <p>Rice Assessment Dashboard</p>
+    </div>
+  );
+}
+
+export default App;
